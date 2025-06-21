ec877af0180c9269463d7184ff9e7ab74d570930
sprinining <sprinining@gmail.com>
2025-06-21 17:26:19 +0800

    perf: 优先使用限定作用域的枚举类型

--- consts.h
+++ consts.h
@@ -3,7 +3,7 @@
 
 #endif // CONSTS_H
 
-enum TreeItemType {
+enum class TreeItemType {
     TreeItemPro = 1, // 条目
     TreeItemDir = 2, // 文件夹
     TreeItemPic = 3  // 图片
@@ -15,7 +15,7 @@ enum TreeItemType {
 // 2. 方便统一管理各种输入错误状态，便于扩展和维护。
 // 3. 避免在多个函数中重复写复杂的判断逻辑，提升代码复用性和一致性。
 // 4. 使用 switch 语句处理不同状态时，代码结构更整洁，易于调试和修改。
-enum InputStatus {
+enum class InputStatus {
     Valid,        // 输入合法，没有错误
     EmptyField,   // 输入字段为空
     PathNotExist, // 指定路径不存在
