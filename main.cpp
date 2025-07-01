6fab4d0b69648ab2d2576a55f7df619068b1cb8a
sprinining <sprinining@gmail.com>
2025-07-01 11:52:50 +0800

    style: 设置样式表

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
     MainWindow w;
+    QFile file(":/style/style.qss");
+    if (file.open(QFile::ReadOnly)) {
+        QString styleSheet = QString::fromUtf8(file.readAll());
+        a.setStyleSheet(styleSheet);
+    }
     w.show();
     return a.exec();
 }
